242e7f7c98cb3f7884da582b2e133f38f0993c54
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2022-09-24 11:37:35 +0530

    Time: 3 ms (64.61%), Space: 8.3 MB (23.53%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int,int> m;
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
         
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++){
-            m[nums[i]]=m[nums[i]]+1;
-        }
-        
-        
-        // while(m[2]!)
-        
-        int hook=0;
-        
-        while(hook!=n){
-            if(m[0]!=0){
-                nums[hook]=0;
-                m[0]=m[0]-1;
-                hook++;
+        while(mid<=high){
+            
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
             }
-            else if(m[1]!=0){
-                nums[hook]=1;
-                m[1]=m[1]-1;
-                hook++;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                nums[hook]=2;
-                m[2]=m[2]-1;
-                hook++;
-                 }
-         }
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
