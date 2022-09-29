19e9f3dd20497d27d785c066e09d129abb4532a2
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2022-09-29 19:34:06 +0530

    Time: 10 ms (32.35%), Space: 9.6 MB (51.16%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
-    int binarySearch(vector<int> arr,int start,int end,int target){
-        int mid=(start+end)/2;
-        
-        if(arr[mid]==target){
-            return target;
-        }
-        
-        if(start>=end){
-            return -1;
-        }
-        
-        
-        
-        int ans;
-        
-        if(target<=arr[mid]){
-            ans=binarySearch(arr,start,mid,target);
-        }
-        
-        if(target>arr[mid]){
-            ans=binarySearch(arr,mid+1,end,target);
-        }
-        
-        return ans;
-        
-        
-    }
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        
         int n=matrix.size();
         int m=matrix[0].size();
+        int i=0;
+        int j=m-1;
         
-        for(int i=0;i<n;i++){
-            int check= binarySearch(matrix[i],0,m-1,target);
+        
+        while(i<n && j>=0){
             
-            cout<<check<<endl;
-            
-            if(check==target){
+            if(matrix[i][j]==target){
                 return true;
             }
             
+            if(matrix[i][j]>target){
+                j--;
+            }else{
+                i++;
+            }
         }
         
-        
         return false;
-        
     }
 };
