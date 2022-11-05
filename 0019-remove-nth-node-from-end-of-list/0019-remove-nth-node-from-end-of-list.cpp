2215325172cfa22a3027318622dcff5f45944884
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2022-11-05 12:38:53 +0530

    Time: 7 ms (69.11%), Space: 10.7 MB (74.90%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,72 +10,34 @@
  */
 class Solution {
 public:
-    ListNode * reverse(ListNode * head){
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * d = new ListNode(-1);
+        d->next=head;
         
-        ListNode * ptr=NULL;
-        ListNode * temp=head;
-        ListNode * curr=temp;
-        
-        while(temp!=NULL){
-            temp=temp->next;
-            curr->next=ptr;
-            ptr=curr;
-            curr=temp;
-        }
-        
-        return ptr;
-        
-    }
-    
-    ListNode * remove(ListNode * head,int n)
-    {
-        if(n==1){
-            return head->next;
-        }
-        if(n==0){
-            return head;
-        }
+        ListNode * f= d;
+        ListNode * s = d;
         
         int cnt=0;
         
-        ListNode * temp=head;
-        ListNode * curr=temp;
-        ListNode * ptr=new ListNode(-1);
-        ListNode * ptrHead=ptr;
-        ptr->next=head;
-        
-        while(temp!=NULL){
-            cnt=cnt+1;
-            
-            if(cnt==n){
-                temp=temp->next;
-                ptr->next=temp;
-                break;
-            }
-            
-            temp=temp->next;
-            ptr=ptr->next;
-        
-            
-            
+        while(cnt!=n){
+            cnt++;
+            f=f->next;
         }
         
-        ptrHead->next=NULL;
+        if(f->next==NULL){
+            d->next=d->next->next;
+            return d->next;
+        }
+        
+        while(f->next!=NULL){
+            f=f->next;
+            s=s->next;
+        }
+        s->next=s->next->next;
+        
+        cout<<f->val<<endl;
+        
         
         return head;
-        
-    }
-    
-    
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        
-        ListNode * rev= reverse(head);
-        
-        ListNode * updatedHead= remove(rev,n);
-        
-        ListNode * ans=reverse(updatedHead);
-        
-        return ans;
     }
 };
