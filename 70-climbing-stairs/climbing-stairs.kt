da8723cc7eaf3e6a0588aa85b21e368500d77ad6
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-05 01:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (74.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.kt
+++ 70-climbing-stairs/climbing-stairs.kt
@@ -2,21 +2,12 @@ class Solution {
     fun climbStairs(n: Int):Int{
         val dp = MutableList(n + 1) { -1 }
 
-        val ans:Int = helper(n,dp)
-        return ans
-    }
+        dp[0] = 1
+        dp[1] = 1
 
-    fun helper(n: Int, dp:MutableList<Int>): Int {
-        if(n==0) return 1
-        if(n<0) return 0
-
-        if(dp[n]!=-1) return dp[n]
-
-        val a:Int = helper(n-1,dp)
-        val b:Int = helper(n-2,dp)
-
-        dp[n] = a+b
-
+        for(i in 2..n){
+            dp[i] = dp[i-1] + dp[i-2]
+        }
         return dp[n]
     }
 }
