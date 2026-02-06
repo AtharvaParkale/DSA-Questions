665db8feb95ea5ec2614cf0ade382bc456360ecd
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-07 02:05:17 +0530

    Time: 1 ms (97.84%) | Memory: 43.6 MB (69.73%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.kt
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.kt
@@ -1,23 +1,14 @@
 class Solution {
-    fun helper(cost:IntArray, i:Int, n:Int,dp:MutableList<Int>):Int{
-        if(i>=n) return 0
-
-        if(dp[i]!=-1) return dp[i]
-
-        val sum1 = helper(cost, i+1,n,dp)
-        val sum2 = helper(cost, i+2,n,dp)
-
-        dp[i] = cost[i] + min(sum1,sum2)
-        return dp[i]
-    }
     fun minCostClimbingStairs(cost: IntArray): Int {
         var ans:Int = Integer.MAX_VALUE
 
-        for(i in 0 .. 1){
-            val dp:MutableList<Int> = MutableList<Int>(cost.size+1){-1}
-            val minCost = helper(cost,i,cost.size, dp)
-            ans = min(minCost,ans)
+        val n = cost.size
+        val dp = IntArray(n + 2)
+
+        for (i in n - 1 downTo 0) {
+            dp[i] = cost[i] + minOf(dp[i + 1], dp[i + 2])
         }
-        return ans  
+
+        return minOf(dp[0], dp[1])
     }
 }
