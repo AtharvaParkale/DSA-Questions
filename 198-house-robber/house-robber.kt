234e2b58f09923dee147652ed90ab7bdad24b627
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-07 02:34:11 +0530

    Time: 1 ms (37.47%) | Memory: 40.4 MB (93.19%) - LeetSync

--- 198-house-robber/house-robber.kt
+++ 198-house-robber/house-robber.kt
@@ -1,20 +1,13 @@
 class Solution {
-    fun helper(nums:IntArray, i:Int,dp:MutableList<Int>):Int{
-        if(i>=nums.size) return 0
-
-        if(dp[i] !=-1) return dp[i]
-
-        val sum1 = nums[i] + helper(nums,i+2,dp)
-        val sum2 = helper(nums,i+1,dp)
-
-        dp[i] = max(sum1,sum2)
-
-        return dp[i]
-    }
 
     fun rob(nums: IntArray): Int {
-        val dp:MutableList<Int> = MutableList<Int>(nums.size+1){-1}
-    
-        return helper(nums,0,dp)
+        val n = nums.size
+        val dp:MutableList<Int> = MutableList<Int>(n+2){0}
+
+        for(i in n-1 downTo 0){
+            dp[i] = max(nums[i] + dp[i+2],dp[i+1] )
+        }
+
+        return dp[0]
     }
 }
