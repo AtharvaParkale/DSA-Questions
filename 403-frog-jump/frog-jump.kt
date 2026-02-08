c128d75168b2524f382673b8d12aec215af0a1be
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-09 02:16:43 +0530

    Time: 418 ms (10.53%) | Memory: 57.2 MB (15.79%) - LeetSync

--- 403-frog-jump/frog-jump.kt
+++ 403-frog-jump/frog-jump.kt
@@ -1,38 +1,34 @@
 class Solution {
 
-    fun recursion(stones: IntArray,i:Int, jump:Int,dp: Array<Array<Boolean?>>):Boolean{
-        if(i>=stones.size) return false
-        if(i== stones.size -1) return true
-
-        if (dp[i][jump] != null) return dp[i][jump]!!
-
-        var jumpList:List<Int> = listOf(1)
-
-        if(i!=0){
-            jumpList = listOf(jump-1,jump,jump+1)
-        }
-
-        var ans = false
-        for(j in jumpList){
-            val num = stones[i] + j
-            if(j<=0 || num<stones[i+1]) {
-                continue
-            }
-            for(k in i+1 .. stones.size -1){
-                if(num == stones[k]){
-                    ans = ans || recursion(stones, k, j,dp)
-                }
-            }
-            
-        }
-
-        dp[i][jump] = ans
-        return ans
-    }
-
     fun canCross(stones: IntArray): Boolean {
         val n = stones.size
-        val dp = Array(n) { Array<Boolean?>(n + 1) { null } }
-        return recursion(stones,0,1,dp)
+        val dp = Array(n) { BooleanArray(n + 1) }
+        dp[0][1] = true
+
+        for (i in 0 until n) {
+            for (jump in 1..n) {
+
+                if (!dp[i][jump]) continue
+                val jumpList =
+                    if (i == 0) listOf(1)
+                    else listOf(jump - 1, jump, jump + 1)
+
+                for (j in jumpList) {
+                    if (j <= 0) continue
+
+                    val nextPos = stones[i] + j
+                    for (k in i + 1 until n) {
+                        if (stones[k] == nextPos) {
+                            dp[k][j] = true
+                        }
+                    }
+                }
+            }
+        }
+        for (jump in 1..n) {
+            if (dp[n - 1][jump]) return true
+        }
+
+        return false
     }
 }
