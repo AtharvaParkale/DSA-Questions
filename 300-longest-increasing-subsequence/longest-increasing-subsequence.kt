7e4900bcd6afd201925d05288f1934bcf1f5de24
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-10 22:19:53 +0530

    Time: 102 ms (10.30%) | Memory: 109.6 MB (6.01%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.kt
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.kt
@@ -1,20 +1,38 @@
 class Solution {
-    fun helper(nums:IntArray, i:Int,prev:Int,dp:MutableList<MutableList<Int>>):Int{
-        if(i>=nums.size) return 0
-        if(dp[i][prev+1] !=-1) return dp[i][prev+1]
 
-        val notTake = 0 + helper(nums,i+1,prev,dp)
-        var take = 0
-        if(prev==-1 || nums[i]>nums[prev]){
-            take = 1 + helper(nums,i+1,i,dp)
+    fun lengthOfLIS(nums: IntArray): Int {
+
+        val n = nums.size
+
+        // dp[i][prevIndex+1]
+        // i → current index
+        // prevIndex → last taken element index (-1 allowed)
+        val dp = Array(n + 1) { IntArray(n + 1) { 0 } }
+
+        // base already 0
+        // dp[n][*] = 0
+
+        // fill from bottom → top
+        for (i in n - 1 downTo 0) {
+
+            // prevIndex can be -1 to i-1
+            for (prevIndex in i - 1 downTo -1) {
+
+                // skip current
+                val notTake = dp[i + 1][prevIndex + 1]
+
+                var take = 0
+
+                // take if increasing
+                if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
+                    take = 1 + dp[i + 1][i + 1]
+                }
+
+                dp[i][prevIndex + 1] = maxOf(take, notTake)
+            }
         }
 
-        dp[i][prev+1] = max(take,notTake)
-        return max(take,notTake)
-    }
-    fun lengthOfLIS(nums: IntArray): Int {
-        val n = nums.size
-        val dp = MutableList(n){MutableList<Int>(n+1){-1}}
-        return helper(nums,0,-1,dp)
+        // start from i=0, prev=-1
+        return dp[0][0]
     }
 }
