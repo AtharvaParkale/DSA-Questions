41d5163e86ef8a3a04a121f635e9b4b31bb43895
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-16 21:14:12 +0530

    Time: 199 ms (37.87%) | Memory: 55 MB (44.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.kt
+++ 200-number-of-islands/number-of-islands.kt
@@ -3,52 +3,41 @@ class Solution {
     val dx:List<Int> = listOf(-1,0,1,0)
     val dy:List<Int> = listOf(0,1,0,-1)
 
-    fun dfs():Int{
-        return -1
-    }
+    var m:Int = 0
+    var n:Int = 0
 
-    fun bfs(grid: Array<CharArray>, vis:MutableList<MutableList<Int>>, i:Int,j:Int){
-        val queue = ArrayDeque<Pair<Int,Int>>()
-        queue.addLast(Pair(i,j))
+    var vis = MutableList(m){MutableList<Int>(n){-1}}
+
+    fun dfs(grid: Array<CharArray>, i:Int,j:Int){
+
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0' || vis[i][j] !=-1){
+            return
+        }
 
         vis[i][j] =1
 
-        val m:Int = grid.size
-        val n:Int = grid[0].size
-
-        while(!queue.isEmpty()){
-            val top = queue.removeFirst()
-
-            val left = top.first
-            val right = top.second
-
-            for(k in 0 until 4){
-                val x = left + dx[k] 
-                val y = right + dy[k]
-
-                if(x>=0 && x<m && y>=0 && y<n && vis[x][y]==-1 && grid[x][y]=='1'){
-                    queue.add(Pair(x,y))
-                    vis[x][y] = 1
-                } 
-            }
+        for(p in 0 until 4){
+            val x = i + dx[p]
+            val y = j + dy[p]
+            dfs(grid,x,y)
         }
+        return 
     }
 
     fun numIslands(grid: Array<CharArray>): Int {
         var ans = 0
 
-        val m:Int = grid.size
-        val n:Int = grid[0].size
-        val nodes: Int = max(m,n)
+        m = grid.size
+        n = grid[0].size
 
-        val vis:MutableList<MutableList<Int>> = MutableList(m){MutableList<Int>(n){-1}}
+        vis = MutableList(m){MutableList<Int>(n){-1}}
 
 
         for(i in 0 until m){
             for(j in 0 until n){
                 if(grid[i][j]=='1' && vis[i][j]==-1){
-                    bfs(grid,vis,i,j)
-                    ans = ans + 1
+                    dfs(grid,i, j)
+                    ans++
                 }
             }
         }
