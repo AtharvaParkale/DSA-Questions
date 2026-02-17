c72f7abb9b85d20365c0283886ab7f8c72a0273d
Atharva Parkale <71985594+AtharvaParkale@users.noreply.github.com>
2026-02-18 00:53:16 +0530

    Time: 33 ms (13.45%) | Memory: 49.9 MB (78.18%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.kt
@@ -0,0 +1,44 @@
+class Solution {
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+                // create a graph
+        val adj:MutableList<MutableList<Int>> = MutableList(numCourses){mutableListOf<Int>()}
+
+        for(it in prerequisites){
+            adj[it[1]].add(it[0])
+        }
+
+        // Indegree Array
+
+        val indegree:MutableList<Int> = MutableList<Int>(numCourses){0}
+
+        for(i in 0 until numCourses){
+            for(it in adj[i]){
+                indegree[it]++
+            }
+        }
+
+        val queue = ArrayDeque<Int>()
+    
+        for(i in 0 until numCourses){
+            if(indegree[i]==0){
+                queue.add(i)
+            }
+        }
+
+        val topo:MutableList<Int> = mutableListOf()
+
+        while(!queue.isEmpty()){
+            val node:Int = queue.removeFirst()
+            topo.add(node)
+
+            for(it in adj[node]){
+                indegree[it]--
+                if(indegree[it]==0) queue.addLast(it)
+            }
+        }
+
+        if(topo.size == numCourses) return topo.toIntArray()
+
+        return intArrayOf()
+    }
+}
